e639198fb8b721b0ac45e38b9b002d08f341cdde
hirimi <89494469+hirimi@users.noreply.github.com>
2021-11-23 23:37:16 +0700

    aziz
    Kosakata JLPT N4 Peta & Lokasi

---
+++ PetaLokasi.kt
@@ -0,0 +1,129 @@
+package com.example.kotoba
+
+object PetaLokasi {
+    fun getKotoba(): ArrayList<N4> {
+        ArrayList<N4>().also {
+            it.apply {
+                add(
+                    N4(
+                        "Tempat",
+                        "ばしょ",
+                        "Basho"
+                    )
+                )
+                add(
+                    N4(
+                        "Gedung Kesenian",
+                        "びじゅつかん",
+                        "BIjutsukan"
+                    )
+                )
+                add(
+                    N4(
+                        "Ilmu Bumi, Geografi",
+                        "ちり",
+                        "Chiri"
+                    )
+                )
+                add(
+                    N4(
+                        "Tempat Parkir",
+                        "ちゅうしゃじょう ",
+                        "Chuushajou"
+                    )
+                )
+                add(
+                    N4(
+                        "Desa, Dusun, Kampung",
+                        "いなか",
+                        "Inaka"
+                    )
+                )
+                add(
+                    N4(
+                        "Pantai, Tepi Laut",
+                        "かいがん",
+                        "Kaigan"
+                    )
+                )
+                add(
+                    N4(
+                        "Ruang Rapat, Tempat Pertemuan",
+                        "かいじょう",
+                        "Kaijou"
+                    )
+                )
+                add(
+                    N4(
+                        "Daerah Sekitar",
+                        "きんじょう",
+                        "kinjou"
+                    )
+                )
+                add(
+                    N4(
+                        "Luar Kota",
+                        "こうがい",
+                        "Kougai"
+                    )
+                )
+                add(
+                    N4(
+                        "Pabrik",
+                        "こうじょう",
+                        "Koujou"
+                    )
+                )
+                add(
+                    N4(
+                        "Bandara Udara, Lapangan Terbang",
+                        "くうこう",
+                        "Kuukou"
+                    )
+                )
+                add(
+                    N4(
+                        "Pelabuhan",
+                        "みなと",
+                        "Minato"
+                    )
+                )
+                add(
+                    N4(
+                        "Penginapan Gaya Jepang",
+                        "りょうかん",
+                        "Ryoukan"
+                    )
+                )
+                add(
+                    N4(
+                        "Kota",
+                        "し",
+                        "shi"
+                    )
+                )
+                add(
+                    N4(
+                        "Pameran",
+                        "てんらんかい",
+                        "Tenrankai"
+                    )
+                )
+                add(
+                    N4(
+                        "Kota Metropolitan, IbuKota",
+                        "と",
+                        "To"
+                    )
+                )
+                add(
+                    N4(
+                        "Tempat Penjualan",
+                        "うりば",
+                        "Uriba"
+                    )
+                )
+            }
+        }
+    }
+}
