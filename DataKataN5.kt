8c9223fc28175c3c9226499bfb4af63d76180ddd
Labib Hiba <57593172+lalabib@users.noreply.github.com>
2021-11-24 03:59:59 +0700

    menambahkan kategori hari

--- DataKataN5.kt
+++ DataKataN5.kt
@@ -5,173 +5,84 @@ object DataKataN5 {
     fun kategoriAngka(): ArrayList<N5> {
         val angka = ArrayList<N5>()
 
-        angka.add(N5(
-            "nomor",
-            "ばん'ごう",
-            "ban'gou"
-        ))
+        angka.add(N5("nomor", "ばん'ごう", "ban'gou"))
 
-        angka.add(N5(
-            "nol",
-            "れい",
-            "rei"
-        ))
+        angka.add(N5("nol", "れい", "rei"))
 
-        angka.add(N5(
-            "nol",
-            "ぜろ",
-            "zero"
-        ))
+        angka.add(N5("nol", "ぜろ", "zero"))
 
-        angka.add(N5(
-            "setengah",
-            "はん",
-            "han"
-        ))
+        angka.add(N5("setengah", "はん", "han"))
 
-        angka.add(N5(
-            "satu",
-            "いち",
-            "ichi"
-        ))
+        angka.add(N5("satu", "いち", "ichi"))
 
-        angka.add(N5(
-            "dua",
-            "に",
-            "ni"
-        ))
+        angka.add(N5("dua", "に", "ni"))
 
-        angka.add(N5(
-            "tiga",
-            "さん",
-            "san"
-        ))
+        angka.add(N5("tiga", "さん", "san"))
 
-        angka.add(N5(
-            "empat",
-            "し, よん",
-            "shi, yon"
-        ))
+        angka.add(N5("empat", "し, よん", "shi, yon"))
 
-        angka.add(N5(
-            "lima",
-            "ご",
-            "go"
-        ))
+        angka.add(N5("lima", "ご", "go"))
 
-        angka.add(N5(
-            "enam",
-            "ろく",
-            "roku"
-        ))
+        angka.add(N5("enam", "ろく", "roku"))
 
-        angka.add(N5(
-            "tujuh",
-            "しち, なな",
-            "shichi, nana"
-        ))
+        angka.add(N5("tujuh", "しち, なな", "shichi, nana"))
 
-        angka.add(N5(
-            "delapan",
-            "はち",
-            "hachi"
-        ))
+        angka.add(N5("delapan", "はち", "hachi"))
 
-        angka.add(N5(
-            "sembilan",
-            "きゅう, く",
-            "kyuu, ku"
-        ))
+        angka.add(N5("sembilan", "きゅう, く", "kyuu, ku"))
 
-        angka.add(N5(
-            "sepuluh",
-            "じゅう",
-            "juu"
-        ))
+        angka.add(N5("sepuluh", "じゅう", "juu"))
 
-        angka.add(N5(
-            "ratus, seratus",
-            "ひゃく",
-            "hyaku"
-        ))
+        angka.add(N5("ratus, seratus", "ひゃく", "hyaku"))
 
-        angka.add(N5(
-            "seribu",
-            "せん",
-            "sen"
-        ))
+        angka.add(N5("seribu", "せん", "sen"))
 
-        angka.add(N5(
-            "sepuluh ribu",
-            "まん",
-            "man"
-        ))
+        angka.add(N5("sepuluh ribu", "まん", "man"))
 
-        angka.add(N5(
-            "1 tahun",
-            "ひとつ",
-            "hitotsu"
-        ))
+        angka.add(N5("1 tahun", "ひとつ", "hitotsu"))
 
-        angka.add(N5(
-            "2 tahun",
-            "ふたつ",
-            "futatsu"
-        ))
+        angka.add(N5("2 tahun", "ふたつ", "futatsu"))
 
-        angka.add(N5(
-            "3 tahun",
-            "みっつ",
-            "mittsu"
-        ))
+        angka.add(N5("3 tahun", "みっつ", "mittsu"))
 
-        angka.add(N5(
-            "4 tahun",
-            "よっつ",
-            "yottsu"
-        ))
+        angka.add(N5("4 tahun", "よっつ", "yottsu"))
 
-        angka.add(N5(
-            "5 tahun",
-            "いつつ",
-            "itsutsu"
-        ))
+        angka.add(N5("5 tahun", "いつつ", "itsutsu"))
 
-        angka.add(N5(
-            "6 tahun",
-            "むっつ",
-            "muttsu"
-        ))
+        angka.add(N5("6 tahun", "むっつ", "muttsu"))
 
-        angka.add(N5(
-            "7 tahun",
-            "ななつ",
-            "nanatsu"
-        ))
+        angka.add(N5("7 tahun", "ななつ", "nanatsu"))
 
-        angka.add(N5(
-            "8 tahun",
-            "やっつ",
-            "yattsu"
-        ))
+        angka.add(N5("8 tahun", "やっつ", "yattsu"))
 
-        angka.add(N5(
-            "9 tahun",
-            "ここのつ",
-            "kokonotsu"
-        ))
+        angka.add(N5("9 tahun", "ここのつ", "kokonotsu"))
 
-        angka.add(N5(
-            "10 tahun",
-            "とお",
-            "too"
-        ))
+        angka.add(N5("10 tahun", "とお", "too"))
 
-        angka.add(N5(
-            "20 tahun",
-            "はたち",
-            "hatachi"
-        ))
+        angka.add(N5("20 tahun", "はたち", "hatachi"))
+
         return angka
+    }
+
+    fun kategoriHari(): ArrayList<N5> {
+        val hari = ArrayList<N5>()
+
+        hari.add(N5("hari", "ようび", "youbi"))
+
+        hari.add(N5("senin", "げつようび", "getsuyoubi"))
+
+        hari.add(N5("selasa", "かようび", "kayoubi"))
+
+        hari.add(N5("rabu", "すいようび", "suiyoubi"))
+
+        hari.add(N5("kamis", "もくようび", "mokuyoubi"))
+
+        hari.add(N5("jumat", "きんようび", "kin'youbi"))
+
+        hari.add(N5("sabtu", "どようび", "doyoubi"))
+
+        hari.add(N5("minggu", "にちようび", "nichiyoubi"))
+
+        return hari
     }
 }
