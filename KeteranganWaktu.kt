309ea8bec80461b02de9a341d7212340885e49cb
hirimi <89494469+hirimi@users.noreply.github.com>
2021-11-25 23:03:51 +0700

    Add files via upload

---
+++ KeteranganWaktu.kt
@@ -0,0 +1,133 @@
+package com.example.kotoba
+
+object KeteranganWaktu {
+    fun getKotoba(): ArrayList<N4> {
+        ArrayList<N4>().also {
+            it.apply {
+                add(
+                    N4(
+                        "Besok, Esok hari",
+                        "あす",
+                        "Asu"
+                    )
+                )
+                add(
+                    N4(
+                        "Situasi, Keadaan",
+                        "ばあい",
+                        "Baai"
+                    )
+                )
+                add(
+                    N4(
+                        "Siang Hari",
+                        "ひるま",
+                        "Hiruma"
+                    )
+                )
+                add(
+                    N4(
+                        "Istirahat Siang",
+                        "ひるやすみ",
+                        "Hiruyasumi"
+                    )
+                )
+                add(
+                    N4(
+                        "Sudah Lama",
+                        "ひさしぶり",
+                        "Hisashiburi"
+                    )
+                )
+                add(
+                    N4(
+                        "Zaman",
+                        "じだい",
+                        "Jidai"
+                    )
+                )
+                add(
+                    N4(
+                        "Nanti, Lain kali, Yang akan datang",
+                        "こんど",
+                        "Kondo"
+                    )
+                )
+                add(
+                    N4(
+                        "Waktu itu",
+                        "このあいだ",
+                        "Konoaida"
+                    )
+                )
+                add(
+                    N4(
+                        "Akhir - Akhir ini",
+                        "このごろ",
+                        "Konogoro"
+                    )
+                )
+                add(
+                    N4(
+                        "Malam ini",
+                        "こんや",
+                        "Kon ya"
+                    )
+                )
+                add(
+                    N4(
+                        "Mulai sekarang, Selanjutnya",
+                        "これから",
+                        "Korekara"
+                    )
+                )
+                add(
+                    N4(
+                        "Mula - mula, Pertama - tama",
+                        "まず",
+                        "Mazu"
+                    )
+                )
+                add(
+                    N4(
+                        "Segera",
+                        "もうすぐ",
+                        "Mousugu"
+                    )
+                )
+                add(
+                    N4(
+                        "Dahulu",
+                        "むかし",
+                        "Mukashi"
+                    )
+                )
+                add(
+                    N4(
+                        "Baru - baru ini, Akhir - Akhir ini",
+                        "さいきん",
+                        "Saikin"
+                    )
+                )
+                add(
+                    N4(
+                        "Tadi, Baru saja",
+                        "さっき",
+                        "Sakki"
+                    )
+                )
+                add(
+                    N4(
+                        "Jarang, Sekali - sekali",
+                        "しばらく",
+                        "Shibaraku"
+                    )
+                )
+                add(
+                    N4(
+                        "Sebentar, Sementara, Agak lama",
+                        "しばらく",
+                        "Shibaraku"
+                    )
+                )
+}
