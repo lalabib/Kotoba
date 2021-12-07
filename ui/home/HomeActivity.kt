60fd4672f1c3877bf7d907a07a6c4b75c416a3b7
Labib Hiba <57593172+lalabib@users.noreply.github.com>
2021-12-07 23:26:16 +0700

    Upload home & main activity
    Menambahkan home dan main activity

---
+++ ui/home/HomeActivity.kt
@@ -0,0 +1,27 @@
+package com.capstone.project.kotoba.ui.home
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.project.kotoba.databinding.ActivityHomeBinding
+import com.capstone.project.kotoba.ui.instruksi.InstruksiFragment
+import com.capstone.project.kotoba.ui.main.MainActivity
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.btnMulai.setOnClickListener {
+            //intent to main
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
