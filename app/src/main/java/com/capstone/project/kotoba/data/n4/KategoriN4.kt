d96e16a83d300d2ed6df3640d42a1c178ce77229
hirimi <azizhilmyhaidar@gmail.com>
2021-12-19 16:56:23 +0700

    menambahkan beberapa perubahan menambahkan tema tampilan warna dan menu setting aplikasi untuk pengingat

--- app/src/main/java/com/capstone/project/kotoba/data/n4/KategoriN4.kt
+++ app/src/main/java/com/capstone/project/kotoba/data/n4/KategoriN4.kt
@@ -1,6 +1,6 @@
 package com.capstone.project.kotoba.data.n4
 
 data class KategoriN4 (
-    var id: Int,
-    var kategori:String
+    val id: Int,
+    val kategori:String
     )
--- app/src/main/java/com/capstone/project/kotoba/ui/kategori/n4/KategoriN4Activity.kt
+++ app/src/main/java/com/capstone/project/kotoba/ui/kategori/n4/KategoriN4Activity.kt
@@ -15,10 +15,13 @@ import com.capstone.project.kotoba.ui.setting.SettingActivity
 
 class KategoriN4Activity : AppCompatActivity() {
     private lateinit var binding: ActivityKategoriN4Binding
+    private val title = "Kategori N4"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityKategoriN4Binding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.title = title
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         val viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())[KategoriN4ViewModel::class.java]
         val kategori = viewModel.getKategoriN4()
 
